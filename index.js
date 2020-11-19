c4a5b4c7a8d71e5dffc28ff203f76e0960a82907
All PGS <kms0219kms@naver.com>
2020-11-19 19:51:52 +0900

    Upload

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+
+const app = express()
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/updates', (req, res) => {
+    res.render('updates')
+})
+
+app.get('/teams/', (req, res) => {
+  res.render('teams/index')
+})
+
+app.get('/teams/about', (req, res) => {
+  res.render('teams/about')
+})
+
+app.get('/teams/contact', (req, res) => {
+  res.render('teams/contact')
+})
+
+app.get('/teams/index', (req, res) => {
+  res.redirect('/teams')
+})
+
+app.get('/app/main', (req, res) => {
+  res.render('app/main')
+})
+
+app.get('/policy/privacy', (req, res) => {
+  res.render('policy/privacy')
+})
+
+app.get('/policy/tos', (req, res) => {
+  res.render('policy/tos')
+})
+
+app.get('/index', (req, res) => {
+  res.redirect('/')
+})
+
+app.get('/support', (req, res) => {
+    res.render('commingsoon')
+    // res.redirect('https://discord.io/kaibot')
+})
+
+app.listen(port, () => console.log(`Listening on ${port}!`))
